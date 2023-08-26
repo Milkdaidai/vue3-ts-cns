7a266731b7c0137e0e8f61e24485bcfab8df30cf
caojingfeng <caojingfengcn@outlook.com>
2023-08-26 14:48:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: caojingfeng caojingfengcn@outlook.com
+ * @Date: 2023-08-26 13:49:29
+ * @LastEditors: caojingfeng caojingfengcn@outlook.com
+ * @LastEditTime: 2023-08-26 14:04:07
+ * @FilePath: /vue3-ts-cns/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your 1Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
